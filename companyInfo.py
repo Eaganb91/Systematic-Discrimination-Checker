73aea8ffcdc2e7cb13e8b1b428c23a99a55b4d85
Gavin Doherty <89102865+GavinDoherty@users.noreply.github.com>
2025-10-04 14:49:23 -0400

    Add files via upload

---
+++ companyInfo.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+file = pd.read_csv(r'thedownloadedgoogleSheetsfile.csv')
+print(file.head())
+
+#avg finders
+meanAge = file["age"].mean()
+meanTenure = file["yearsAtCompany"].mean()
+
+#National Compare
+
+medianPay = file["pay"].median()
+
+womanSum = (file["gender"] == "W").sum()
+genderSum = len(file["gender"])
+womanDistribution = (womanSum / genderSum) * 100
+
+
+print(f"Gender Split {womanDistribution:.2f}%")
+
+avgPayMen = file.loc[file["gender"] == "M", "pay"].mean()
+avgPayWomen = file.loc[file["gender"] == "W", "pay"].mean()
+
+payGap = ((avgPayMen - avgPayWomen) / avgPayMen) * 100
+
+
